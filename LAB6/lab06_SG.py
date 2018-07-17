198ce757c245a7afd6d40af8c8a227f7d2d913ca
seb1gonzalez <sebastian95gonzalez@gmail.com>
2018-07-16 19:22:13 -0600

    Solved problem 1, kudos

--- LAB6/lab06_SG.py
+++ LAB6/lab06_SG.py
@@ -1,6 +1,6 @@
 import re
 password = input("Please enter a password")
-if re.search(r'[A-Za-z0-9#$%^&*+=\-\_\!]{6,}',password):
+if re.match(r'{[A-Za-z0-9#$%^&*+=\-\_\!]+}{6,}',password):
     print ("Success!")
 else:
     print ("Self-destroying")
