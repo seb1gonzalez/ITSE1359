e9c5115a96c007f12e7288a232f181cf157e202e
seb1gonzalez <sebastian95gonzalez@gmail.com>
2018-07-17 11:58:31 -0600

    Problem 2, solved

--- LAB6/lab06_SG.py
+++ LAB6/lab06_SG.py
@@ -1,8 +1,16 @@
 import re
 password = input("Please enter a password\n")
-
+#-------------------For Problem 1---------------------
+# if re.search(r'([\d\w][#$%^&*\-\_\!])+',password):
+#     if len(password) > 5:
+#         print ("Success!")
+# else:
+#     print ("Self-destroying")
+#-------------------For Problem 2---------------------
 if re.search(r'([\d\w][#$%^&*\-\_\!])+',password):
-    if len(password) > 5:
-        print ("Success!")
+    if len(password) > 7:
+        print ("Success! Your password is strong enough")
+    else:
+        print ("YOUR PASSWORD IS NOT LONG ENOUGH\n Enter a password of at least 8 characters")
 else:
-    print ("Self-destroying")
+    print ("Not a valid password")
