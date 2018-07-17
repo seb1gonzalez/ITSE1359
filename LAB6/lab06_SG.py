043c899818dbae50251a5c3c725aec8e692b7e47
seb1gonzalez <sebastian95gonzalez@gmail.com>
2018-07-17 11:44:13 -0600

    Solved problem 1, kudos

--- LAB6/lab06_SG.py
+++ LAB6/lab06_SG.py
@@ -1,6 +1,8 @@
 import re
-password = input("Please enter a password")
-if re.match(r'{[A-Za-z0-9#$%^&*+=\-\_\!]+}{6,}',password):
-    print ("Success!")
+password = input("Please enter a password\n")
+my_regex = "[A-Za-z0-9#$%^&*+=\-\_\!]"
+if re.search(r'([\d\w][#$%^&*\-\_\!])+',password):
+    if len(password) > 5:
+        print ("Success!")
 else:
     print ("Self-destroying")
